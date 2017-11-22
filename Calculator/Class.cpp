434f1abc2fe77cf7c39aac2a2d3e5aecafbd59d6
Glumac7 <necaglumicic@gmail.com>
2017-11-22 22:42:29 +0100

    Progressing
    I made some stuff work but having some small problems with signs(+, -, *, /). Will fix.

--- Calculator/Class.cpp
+++ Calculator/Class.cpp
@@ -6,55 +6,66 @@ void Class::input(std::string str)
 	std::cout << "Enter some numbers with signes in between them(+, -, *, /): ";
 	std::getline(std::cin, str);
 
-	m_String_input =  str;
+	m_String_input =  str + " ";
 }
 
 void Class::getSplit_input()
 {
-	int  str_to_int = 0, al, d = 1;
-	bool flag = false;
+	int  str_to_int = 0, multiplier = 1, end_value = 0;
+	int k = 1, l = 0, first_num = 0;
+	bool flag;
 	std::string str;
 
-	int k = 1, l = 0, dd = 0;
+	first_num = std::stoi(m_String_input); //Gets the very firstcolection of numbers
+	m_Vector_int.push_back(first_num); //And puts them in a std::vector<int>
 
-	dd = std::stoi(m_String_input);
-	m_Vector_int.push_back(dd);
+	for (int i = 0; i < m_String_input.size(); i++) //While we don't get to the end
+	{
+		l = i + 1;
+		if ((m_String_input.at(i) == '+' || m_String_input.at(i) == '-' || m_String_input.at(i) == '*' ||
+			m_String_input.at(i) == '/') && m_String_input.at(l) == ' ')
+		{
+			int j = l + 1;
+			while (m_String_input.at(j) != ' ') //'345' goes from 3 to the space after 5
+			{
+				j++;
+			}
 
-	/*for (int i = 0;i <= m_String_input.size(); i++)
-	  {
-		  std::cout << i;
-		  if (m_String_input.at(i) == ' ')
-		  {
-			  l = i + 1;
-			  if (m_String_input.at(l) == '+' || m_String_input.at(l) == '-' || m_String_input.at(l) == '*' || m_String_input.at(l) == '/')
-			  {
-				  for (int j = (i - 1); j >= 0; j--)
-				  {
-					  if (!flag)
-					  {
-						  flag = true;
-						  str = m_String_input.at(j);
-						  str_to_int = std::stoi(str);
-						  al = str_to_int;
-					  }
-					  else
-					  {
-						  str = m_String_input.at(j);
-						  str_to_int = std::stoi(str);
-						  k = str_to_int;
-						  d *= 10;
-						  k *= d;
-						  al += k;
+			j -= 1; //Eliminatess the space
 
-					  }
+			flag = false;
+			while (j != l)  // Goes from 5 to 3 and does some calculations
+			{
+				if (!flag)
+				{
+					flag = true;
+					str = m_String_input.at(j);
+					str_to_int = std::stoi(str);
+					end_value = str_to_int;
+				}
+				else
+				{
+					str = m_String_input.at(j);
+					str_to_int = std::stoi(str);
+					k = str_to_int;
+					multiplier *= 10;
+					k *= multiplier;
+					end_value += k;
+				}
+				j--;
+			}
 
+			m_Vector_int.push_back(end_value); //Pushes the end_value
+			m_Vector_char.push_back(i); //Pushes the sign(+, -, *, /)
 
-					  m_Vector_int.push_back(al);
-					  m_Vector_char.push_back(l);
-				  }
-			  }
-		  }
-	}*/
+										//Resets the values
+			end_value = 0;
+			k = 0;
+			multiplier = 1;
+			str_to_int = 0;
+
+		}
+	}
 }
 
 double Class::calculate()
