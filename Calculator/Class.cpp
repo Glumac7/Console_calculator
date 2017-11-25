45d4a2e36aa0ce6b553509fab72a420611059409
Glumac7 <necaglumicic@gmail.com>
2017-11-25 14:59:07 +0100

    Close to the end.
    I'm close to the end, just some small stuff to fix and it will be done...tomorrow hopefully.

--- Calculator/Class.cpp
+++ Calculator/Class.cpp
@@ -71,116 +71,112 @@ void Class::getSplit_input()
 double Class::calculate()
 {
 	double result = 0;
-	double midel_var1;
-	double midel_var2;
-	double midel_var3;
-	double midel_var4;
+	double middle_var1;
+	double middle_var2;
+	double middle_var3;
+	double middle_var4;
 	int vector_int_index = 0;
+	int j;
 
-	midel_var1 = double(m_Vector_int.at(vector_int_index));
-	midel_var2 = double(m_Vector_int.at(vector_int_index + 1));
+	
 
 	for (int i = 0; i < m_Vector_char.size(); i++) //Goes from the first to the last char in a vector(m_Vecotr_char)
 	{
-		
+		middle_var1 = double(m_Vector_int.at(vector_int_index));
+		middle_var2 = double(m_Vector_int.at(vector_int_index + 1));
+		j = i;
 		switch (m_Vector_char.at(i))
 		{
 		case '+': //If 'm_Vector_char' that is being incremented by 'i' is '+' then...
-			if (m_Vector_char.at(i + 1) == '*' || m_Vector_char.at(i + 1) == '/') //...it checks if the next index is '*' or '/'
+			if (m_Vector_char.size() > 1 && i < m_Vector_char.size() - 1)
 			{
-				break;
+				if (m_Vector_char.at(i + 1) == '*' || m_Vector_char.at(i + 1) == '/') //...it checks if the next index is '*' or '/'
+				{
+					break;
+				}
 			}
-			else if (i == 0) // If it's not then it check to see if 'i' is the first char in the program
+			
+			if (i == 0) // If it's not then it check to see if 'i' is the first char in the program
 			{
-				result = midel_var1 + midel_var2; //If it is then the result is the value before + the value after '+'
+				result = middle_var1 + middle_var2; //If it is then the result is the value before + the value after '+'
 			}
 			else
-				result += midel_var2;//If it's not the first sign in the program; the result is the result + the number after '+'
+				result += middle_var2;//If it's not the first sign in the program; the result is the result + the number after '+'
 			break;
 
 		case '-'://Same as the '+' but this time it's '-'
-			if (m_Vector_char.at(i + 1) == '*' || m_Vector_char.at(i + 1) == '/')
+			if (m_Vector_char.size() > 1 && i < m_Vector_char.size() - 1)
 			{
-				break;
+				if (m_Vector_char.at(i + 1) == '*' || m_Vector_char.at(i + 1) == '/')
+				{
+					break;
+				}
 			}
+			
 			if (i == 0)
 			{
-				result = midel_var1 - midel_var2;
+				result = middle_var1 - middle_var2;
 			}
 			else
-				result -= midel_var2;
+				result -= middle_var2;
 			break;
 
 		case '*'://If 'm_Vector_char' that is being incremented by 'i' is '*' then...
-			if (i >= 1)//...it checks to see if it's the first char in a program or not
+			middle_var3 = middle_var1 * middle_var2;
+			if (i >= 0 && i < m_Vector_char.size() - 1)//...it checks to see if it's the first char in a program or not
 			{
-				midel_var3 = midel_var1 * midel_var2;//If it is then the 'midel_var3' is the result of a multiplication of an element before and an element after '*'
-				midel_var2 = double(m_Vector_int.at(vector_int_index - 1));
-
-				for (int j = 0; j < m_Vector_char.size(); j++)
+				if (m_Vector_char.at(i + 1) == '*')
 				{
-					if (m_Vector_char.at(i + (j + 1)) == '*')
+					while (m_Vector_char.at(j) == '*' && j != m_Vector_char.size()-1)
 					{
-						midel_var1 = double(m_Vector_int.at(vector_int_index + j));
-						midel_var2 = double(m_Vector_int.at(vector_int_index + (j + 1)));
-
-						midel_var4 = midel_var1 * midel_var2;
-						result += midel_var4;
+						middle_var3 *= middle_var2;
+						j++;
 					}
-					else if(m_Vector_char.at(i + (j + 1)) == '/')
-					{
-						midel_var1 = double(m_Vector_int.at(vector_int_index + j));
-						midel_var2 = double(m_Vector_int.at(vector_int_index + (j + 1)));
-
-						midel_var4 = midel_var1 / midel_var2;
-						result += midel_var4;
-					}
-					else
-						break;
-				}
-
-				vector_int_index = i;
-
-				//Checks to see what is the caracter before '*'
-				if (m_Vector_char.at(i - 1) == '+')
-				{
-					midel_var4 = midel_var2 + midel_var3;
 
 				}
-
-				if (m_Vector_char.at(i - 1) == '-')
-				{
-					midel_var4 = midel_var2 - midel_var3;
-					result += midel_var4;
-				}
-
-				if (m_Vector_char.at(i - 1) == '*')
-				{
-					midel_var2 = double(m_Vector_int.at(vector_int_index + 1));
-					result *= midel_var2;
-					
-				}
-
-				if (m_Vector_char.at(i - 1) == '/')
-				{
-					midel_var2 = double(m_Vector_int.at(vector_int_index + 1));
-					result *= midel_var2;
-				}
+				
 			}
 			else
 			{
-				result = midel_var1 * midel_var2;
+				if (i == 1)
+				{
+					middle_var2 = double(m_Vector_int.at(vector_int_index - 1));
+					if (m_Vector_char.at(i - 1) == '+')
+					{
+						result = middle_var2;
+						result += middle_var3;
+					}
+
+					if (m_Vector_char.at(i - 1) == '-')
+					{
+						result = middle_var2;
+						result -= middle_var3;
+					}
+				}
+				else if (m_Vector_char.size() > 1)
+				{
+					if (m_Vector_char.at(i - 1) == '+')
+					{
+						result += middle_var3;
+					}
+
+					if (m_Vector_char.at(i - 1) == '-')
+					{
+						result -= middle_var3;
+					}
+				}
+				else
+					result = middle_var3;
 			}
-			
 			break;
 
 		case '/':
 			if (i == 0)
 			{
-				result = midel_var1 / midel_var2;
+				result = middle_var1 / middle_var2;
 			}
 			else
-				result /= midel_var2;
+				result /= middle_var2;
 			break;
 
 		default:
